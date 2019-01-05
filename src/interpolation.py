0d971309caa4ab540932d75e460ae8efed1ad4c0
PierreGtch <pierre.guetschel@gmail.com>
2019-01-05 13:13:25 +0100

    functions to interpolate lists of points by polynoms

---
+++ src/interpolation.py
@@ -0,0 +1,144 @@
+import numpy as np
+from scipy.interpolate import lagrange
+from math import ceil, floor
+
+
+def to_funct(intervals, polynoms, raiseValueError):
+    ''' if the value is out of the intervals and raiseValueError=True,
+    then raises a ValueError. If raiseValueError=False,
+    the returned function is false after and before the intervals. '''
+    def dico(x, i0, i1):
+        if i0+1 == i1:
+            return polynoms[i0]
+        imid = floor((i0+i1)/2)
+        if x>intervals[imid]:
+            return dico(x, imid, i1)
+        else:
+            return dico(x, i0, imid)
+
+    x_min = intervals[0]
+    x_max = intervals[-1]
+    def funct(x):
+        if raiseValueError and (x<x_min or x>x_max):
+            raise ValueError('function defined between {} and {} (not in {})'.format(x_min, x_max, x))
+        elif x<x_min:
+            return polynoms[0](x_min)
+        elif x>x_max:
+            return polynoms[-1](x_max)
+
+        p = dico(x, 0, len(intervals)-1)
+        return p(x)
+
+    return funct
+
+def interpolate_1d(x, y, d, testing=False, raiseValueError=False):
+    ''' Givet two lists x and y s.t. f(x)=y,
+    returns a continious function composed of polynoms of degree d'''
+    assert isinstance(d,int) and d >= 1
+    x = np.array(x)
+    y = np.array(y)
+    assert x.shape==y.shape
+    assert x.ndim==1
+    before = 1 - ceil(d/2)
+    after = 1 + floor(d/2)
+    first = -before
+    last = len(x)-after+1
+
+    polynoms = []
+    for i in range(first, last):
+        xi = x[i+before:i+after+1]
+        yi = y[i+before:i+after+1]
+        pi = lagrange(xi, yi)
+        polynoms.append(pi)
+
+    intervals = x[first:last].copy()
+    intervals[0] = x[0]
+    intervals[-1] = x[-1]
+
+    funct = to_funct(intervals, polynoms, raiseValueError=raiseValueError)
+    if testing:
+        return funct, intervals, polynoms
+    return funct
+
+def interpolate_nd(t, points, d, raiseValueError=False):
+    ''' Givet two lists t and points
+    s.t. points is a list of vectors of the same dim
+    returns a continious function composed of polynoms of degree d'''
+    assert isinstance(d,int) and d >= 1
+    t = np.array(t)
+    points = np.array(points)
+    assert t.shape[0]==points.shape[0]
+    assert t.ndim==1
+
+    output_shape = points[0].shape
+    outputs = {}
+    for i in np.ndindex(output_shape):
+        y = np.array([x[i] for x in points])
+        f = interpolate_1d(t, y, d, testing=False, raiseValueError=raiseValueError)
+        outputs[i] = f
+
+    def func(x):
+        out = np.zeros(output_shape)
+        for i in np.ndindex(output_shape):
+            out[i] = outputs[i](x)
+        return out
+    return func
+
+
+#------------------------------------------------
+
+def test_1d(n,d_max):
+    X = np.linspace(0,1,num=n)
+    precision = 10
+    X_details = np.linspace(0,1, num=n*precision)
+    Y = np.random.normal(size=X.shape)
+    for i in range(1,n):
+        Y[i] += Y[i-1]
+
+    Y_details = []
+    for i in range(1,d_max+1):
+        f = interpolate_1d(X,Y,i)
+        y = [f(x) for x in X_details]
+        Y_details.append((i,np.array(y)))
+
+    import matplotlib.pyplot as plt
+    plt.figure(1)
+    plt.plot(X,Y, 'o')
+
+    for i,y in Y_details:
+        plt.plot(X_details, y, label=str(i))
+
+    plt.legend()
+    plt.show()
+
+def test_2d(n,d_max):
+    dims = 2
+    X = np.linspace(0,1,num=n)
+    precision = 10
+    X_details = np.linspace(0,1, num=n*precision)
+    Y = np.random.normal(size=(*X.shape,dims))
+    for i in range(1,n):
+        Y[i] += Y[i-1]
+
+    Y_details = []
+    for i in range(1,d_max+1):
+        f = interpolate_nd(X,Y,i)
+        y = [f(x) for x in X_details]
+        Y_details.append((i,np.array(y)))
+
+    import matplotlib.pyplot as plt
+    plt.figure(1)
+    plt.plot(Y[:,0],Y[:,1], 'o')
+
+    for i,y in Y_details:
+        plt.plot(y[:,0],y[:,1], label=str(i))
+
+    plt.legend()
+    plt.show()
+
+
+#----------------------------------------------
+
+
+test_1d(8, 5)
+test_2d(15, 3)
