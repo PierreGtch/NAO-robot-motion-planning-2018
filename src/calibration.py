581e0d83aaed3eb770795048b5c561759a8a6b44
Marius Dufraisse <marius.dufraisse@gmail.com>
2019-01-12 19:52:14 +0100

    Rends le bras mou pour la calibration

--- src/calibration.py
+++ src/calibration.py
@@ -8,6 +8,7 @@ import sys
 import time
 from naoqi import ALProxy
 import motion
+import textwrap
 
 if sys.version_info.major == 2:
     # We are using Python 2.x
@@ -28,18 +29,36 @@ def get_xyz(motionProxy, effector="LArm", frame=motion.FRAME_TORSO, useSensorVal
     full_position = motionProxy.getPosition(effector,frame,useSensorValues)
     return full_position[:3]
 
+def get_calibration_data(proxy, effector="LArm"):
+    """
+    Ask the user for free points in order to define the drawing space.
+
+    :param proxy: The motionProxy to use.
+    :param effector: The arm we will use.
+    :return: A list of three 3D-Points
+    """
+    messagebox.showinfo("Calibration", textwrap.fill("""Veuillez prendre \
+    la main gauche du robot NAO"""))
+    proxy.setStiffnesses(effector, 0.0)
+    messagebox.showinfo("Calibration (1/3)", textwrap.fill("""Maintenez le bras\
+    gauche du robot en place et appuyer sur entrée."""))
+    p1 = get_xyz(proxy,effector)
+    messagebox.showinfo("Calibration (2/3)", textwrap.fill("""Maintenez le bras\
+    gauche du robot en place et appuyer sur entrée."""))
+    p2 = get_xyz(proxy,effector)
+    messagebox.showinfo("Calibration (3/3)", textwrap.fill("""Maintenez le bras\
+    gauche du robot en place et appuyer sur entrée."""))
+    p3 = get_xyz(proxy,effector)
+    proxy.setStiffnesses(effector, 1)
+    return [p1,p2,p3]
+
+
 if __name__=='__main__':
     robotIP = '127.0.0.1'
-    PORT = 9559
+    PORT = 54010
     proxy = ALProxy("ALMotion",robotIP,PORT)
 
-
-    messagebox.showinfo("Calibration (1/3)", "Maintenez le bras gauche du robot en place et appuyer sur entrée.")
-    p1 = get_xyz(proxy)
-    messagebox.showinfo("Calibration (2/3)", "Maintenez le bras gauche du robot en place et appuyer sur entrée.")
-    p2 = get_xyz(proxy)
-    messagebox.showinfo("Calibration (3/3)", "Maintenez le bras gauche du robot en place et appuyer sur entrée.")
-    p3 = get_xyz(proxy)
+    p1, p2, p3 = get_calibration_data(proxy)
     print("X   Y   Z")
     print(p1)
     print(p2)
