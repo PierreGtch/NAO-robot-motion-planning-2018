67ee4522aa983dfd4e7b8597adcac1ebb9dd3b96
Marius Dufraisse <marius.dufraisse@gmail.com>
2019-01-14 23:08:01 +0100

    Add a class to convert 2D points to 3D points as well as tests

---
+++ src/UpDim.py
@@ -0,0 +1,37 @@
+# coding: utf8
+
+"""
+	Converion from the 2D plane to the 3D plane
+"""
+
+import numpy as np
+
+class UpDim():
+	"""
+		A class which provide method to convert 2D coordinates in a plane to 3D coordinates.
+	"""
+	def __init__(self,origin,e1,e2,x_max,y_max):
+		"""
+		:param origin: An arry with the coordinates of a  point on the target point which will be the translation of the point (0,0) from the 2D space.
+		:param e1: An array for a basis of the 3D plane.
+		:param e2: An array for a basis of the 3D plane.		
+		:param x_max: A float describing how far we can go in 3D along e1.		
+		:param y_max: A float describing how far we can go in 3D along e2.		
+		"""
+		self.origin	= origin
+		self.e1 = e1
+		self.e2 = e2
+		self.x_max = x_max
+		self.y_max = y_max
+
+	def convert(self,x,y):
+		"""
+			Convertit un point du plan 2D vers le plan 3D.
+		:param x: La coordonnée en x.
+		:param y: La coordonnée en y.
+		:return : Un array contenant les coordonnées (x,y,z) du point 3D.
+		"""
+		delta_x =  x * self.x_max * self.e1
+		delta_y =  y * self.y_max * self.e2
+		return self.origin + delta_x + delta_y
+
---
+++ src/UpDimTest.py
@@ -0,0 +1,40 @@
+# coding: utf8
+
+"""
+	Unittests for UpDim.py
+"""
+
+import unittest
+import numpy as np
+
+from UpDim import UpDim
+
+class Test(unittest.TestCase):
+	"""
+	A class to test the module UpDim
+	"""
+	
+	def test_init(self):
+		try :
+			origin = np.array([0,0,0])
+			e1 = np.array([0,0,0])
+			e2 = np.array([0,0,0])
+			x_max = 42
+			y_max = 42			
+			a = UpDim(origin,e1,e2,x_max,y_max)
+		except:
+			self.assertEqual("","Problem during initialisation")
+
+	def test_output(self):
+		origin = np.array([0,0,0])
+		e1 = np.array([1,0,0])
+		e2 = np.array([0,1,-1])
+		x_max = 10
+		y_max = 10			
+		c = UpDim(origin,e1,e2,x_max,y_max)
+		out = c.convert(0.1,0.5)
+		self.assertEqual(np.all(out == np.array([1,5,-5])),True)
+
+if __name__=="__main__":
+	unittest.main()
+
